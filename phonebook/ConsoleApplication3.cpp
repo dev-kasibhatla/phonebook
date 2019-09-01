d10befb1fe98e97d12e25a4834e3052387e0ba83
Pruthav Sanwatsarkar <pruthavs@gmail.com>
2019-09-01 06:29:23 +0530

    able to save contacts but not added obj hidden files yet.

--- phonebook/ConsoleApplication3.cpp
+++ phonebook/ConsoleApplication3.cpp
@@ -2,11 +2,11 @@
 //
 
 //============================================================================
-// Name        : readwrite.cpp
+// Name        : phonebook.cpp
 // Author      : pruthav
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : attempt for phonebook database
 //============================================================================
 
 #include <iostream>
@@ -31,7 +31,7 @@ int main() {
 	vector <string> city;
 	vector <string> state;
 	vector <string> email;
-	
+
 
 
 	string str;
@@ -41,85 +41,146 @@ int main() {
 		lines++;
 		//cout << str << endl;
 		stringstream s(str);
-		
-		while (getline(s, temp, ',' )) {
+
+		while (getline(s, temp, ',')) {
 
 			// add all the column data 
 			// of a row to a vector 
 			switch (count)
-			{	
-				case 0:
-				{
-					first_name.push_back(temp);
-					count++;
-					break;
-				}
-				case 1:
-				{
-					last_name.push_back(temp);
-					count++;
-					break;
-				}
-				case 2:
-				{
-					phone.push_back(temp);
-					count++;
-					break;
-				}
-				case 3:
-				{
-					email.push_back(temp);
-					count++;
-					break;
-				}
-				case 4:
-				{
-					address.push_back(temp);
-					count++;
-					break;
-				}
-				case 5:
-				{
-					city.push_back(temp);
-					count++;
-					break;
-				}
-				case 6:
-				{
-					state.push_back(temp);
-					count = 0;
-					break;
-				}
-				default:
-					break;
+			{
+			case 0:
+			{
+				first_name.push_back(temp);
+				count++;
+				break;
 			}
-			
-			
-			
+			case 1:
+			{
+				last_name.push_back(temp);
+				count++;
+				break;
+			}
+			case 2:
+			{
+				phone.push_back(temp);
+				count++;
+				break;
+			}
+			case 3:
+			{
+				email.push_back(temp);
+				count++;
+				break;
+			}
+			case 4:
+			{
+				address.push_back(temp);
+				count++;
+				break;
+			}
+			case 5:
+			{
+				city.push_back(temp);
+				count++;
+				break;
+			}
+			case 6:
+			{
+				state.push_back(temp);
+				count = 0;
+				break;
+			}
+			default:
+				break;
+			}
+
+
+
 		}
 
 	}
-	cout << "List of names " << endl<<endl<<endl<<endl;
-	for (int i = 0; i < lines; i++) {
-		cout << first_name[i]<<endl;
+
+
+	char option;
+	cout << "do u want to save a contact? (y/n): ";
+	cin >> option;
+	switch (option)
+	{
+	case 'y':
+	{
+		cout << "enter details of ur contact : " << endl;
+		cout << "first name ---> ";
+		cin >> temp;
+		first_name.push_back(temp);
+		cout << endl;
+
+
+		cout << "last name ---> ";
+		cin >> temp;
+		last_name.push_back(temp);
+		cout << endl;
+
+
+		cout << "addr ---> ";
+		cin >> temp;
+		address.push_back(temp);
+		cout << endl;
+
+
+		cout << "phone ---> ";
+		cin >> temp;
+		phone.push_back(temp);
+		cout << endl;
+
+
+		cout << "email ---> ";
+		cin >> temp;
+		email.push_back(temp);
+		cout << endl;
+
+
+		cout << "state ---> ";
+		cin >> temp;
+		state.push_back(temp);
+		cout << endl;
+
+
+		cout << "city ---> ";
+		cin >> temp;
+		city.push_back(temp);
+		cout << endl;
+		lines++;
+		break;
 	}
-	cout << "List of phones " << endl<< endl << endl << endl;
+	case 'n':
+	{
+		break;
+	}
+
+
+	}
+
+	cout << "List of first names " << endl << endl << endl << endl;
+	for (int i = 0; i < lines; i++) {
+		cout << first_name[i] << endl;
+	}
+	cout << "List of phones " << endl << endl << endl << endl;
 	for (int i = 0; i < lines; i++) {
 		cout << phone[i] << endl;
 	}
-	cout << "List of  last names " << endl<< endl << endl << endl;
+	cout << "List of  last names " << endl << endl << endl << endl;
 	for (int i = 0; i < lines; i++) {
 		cout << last_name[i] << endl;
 	}
-	cout << "List of email" << endl<< endl << endl << endl;
+	cout << "List of email" << endl << endl << endl << endl;
 	for (int i = 0; i < lines; i++) {
 		cout << email[i] << endl;
 	}
-	cout << "List of address " << endl<< endl << endl << endl;
+	cout << "List of address " << endl << endl << endl << endl;
 	for (int i = 0; i < lines; i++) {
 		cout << address[i] << endl;
 	}
-	cout << "List of states " << endl<< endl << endl << endl;
+	cout << "List of states " << endl << endl << endl << endl;
 	for (int i = 0; i < lines; i++) {
 		cout << state[i] << endl;
 	}
@@ -127,8 +188,6 @@ int main() {
 	for (int i = 0; i < lines; i++) {
 		cout << city[i] << endl;
 	}
-	
-	
 
 	return 0;
 }
@@ -140,7 +199,7 @@ int main() {
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	std::cout << "Hello World!\n";
 }
 */
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
