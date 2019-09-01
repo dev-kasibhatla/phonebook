76553552b36e316ea832ace490c2b62db8d084bb
Aditya Kasibhatla <dev.kasibhatla@gmail.com>
2019-09-01 23:17:06 +0530

    load data

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "Klog.cpp"
+#include "DataProvider.cpp"
 using namespace std;
 
 int main(){
 	Klog logger;
 	logger.clear();
+	DataProvider dataProvider;
+	dataProvider.load();	
 	return 0;
 }
